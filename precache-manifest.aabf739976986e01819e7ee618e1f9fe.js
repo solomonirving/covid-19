40a67055594a85e3d439e5f93a9c288c49d4a8cf
Solomon Irving <solomon.irving@gmail.com>
2020-04-01 13:02:47 -0700

    Updates

---
+++ precache-manifest.aabf739976986e01819e7ee618e1f9fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09af92925fc84bddd0c75d67280fe5e",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "bef4afabae5b325f8f8a",
+    "url": "/covid-19/static/css/main.de3ef2c6.chunk.css"
+  },
+  {
+    "revision": "abf154e0ac575d802bc8",
+    "url": "/covid-19/static/js/2.fa027e20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19/static/js/2.fa027e20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bef4afabae5b325f8f8a",
+    "url": "/covid-19/static/js/main.47973634.chunk.js"
+  },
+  {
+    "revision": "b08d7cd09db0e64ee56d",
+    "url": "/covid-19/static/js/runtime-main.09859d37.js"
+  }
+]);
